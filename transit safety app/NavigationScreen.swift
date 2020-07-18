7733b8f75bae1a9631862b49a1c12db4e8178b79
OllieHarman97 <oh2212@columbia.edu>
2020-07-18 19:32:32 +0100

    Added three screens (Main Menu, Create Account, Map View sans Google Maps api)

---
+++ transit safety app/NavigationScreen.swift
@@ -0,0 +1,21 @@
+//
+//  NavigationScreen.swift
+//  transit safety app
+//
+//  Created by Oliver Harman on 15/07/2020.
+//  Copyright © 2020 Shannon Jin. All rights reserved.
+//
+
+import SwiftUI
+
+struct NavigationScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NavigationScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationScreen()
+    }
+}
